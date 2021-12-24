999d941d4f23d5fb5325409149062d20e6e8fcb6
Dginozator <viz@dginozator.com>
2021-12-24 12:16:54 +0300

    v0_0_2

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,11 @@
 <script>
   import "../app.css";
+  import { mode } from '$app/env';
 </script>
 
+{#if mode != 'production'}
+  <p>hrom</p>
+  <script src="https://cdn.tailwindcss.com"></script>
+{/if}
 <slot />
+<p>{mode}</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="font-serif text-2xl text-blue-700">test2!</p>
